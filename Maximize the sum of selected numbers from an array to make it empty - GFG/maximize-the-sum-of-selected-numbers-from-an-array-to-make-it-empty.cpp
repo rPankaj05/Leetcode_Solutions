c69251e99c39c6f66e43605b680f8ed2f6ac3834
rPankaj05 <108936363+rPankaj05@users.noreply.github.com>
2022-10-14 00:11:35 +0530

    Added solution - LeetHub

---
+++ Maximize the sum of selected numbers from an array to make it empty - GFG/maximize-the-sum-of-selected-numbers-from-an-array-to-make-it-empty.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int maximizeSum(int a[], int n) 
+    {
+        sort(a,a+n);
+        map<int,int> mp;
+        for(int i=0;i<n;i++) mp[a[i]]++;
+        int ans=0;
+        for(int i=n-1;i>=0;i--){
+            if(mp[a[i]]>0){
+                ans+=a[i];
+                mp[a[i]]--;
+                if(mp[a[i]-1]>0) {
+                    mp[a[i]-1]--;
+                }
+            }
+        }
+        return ans;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+
+
+int main()
+{
+    
+    int t;cin>> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        sort(arr, arr+n);
+        Solution ob;
+        
+        cout << ob.maximizeSum(arr, n) << endl;
+        
+        
+    }
+
+}
+
+
+// } Driver Code Ends
