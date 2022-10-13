3b4adb512fe2a091a8c4dcdbc84bd3a77461d8b9
rPankaj05 <108936363+rPankaj05@users.noreply.github.com>
2022-10-14 03:08:34 +0530

    Time: 15 ms (73.57%), Space: 12.4 MB (7.85%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -20,8 +20,11 @@ public:
         
         while(curr!=NULL){
             if(mp[curr->val]==1){
+                ListNode* temp=curr;
                 curr=curr->next;
                 prev->next=curr;
+                temp->next=NULL;
+                delete temp;
             }
             
             else{
