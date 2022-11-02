2a09cd4e7468c7d20fb1d4fad67cb808ed4baadb
rPankaj05 <108936363+rPankaj05@users.noreply.github.com>
2022-11-02 12:49:58 +0530

    Time: 485 ms (24.76%), Space: 74 MB (40.58%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -8,6 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+/*
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
@@ -26,5 +28,64 @@ public:
             
         }
         return head;
+    }
+};
+
+*/
+
+class Solution {
+public:
+     ListNode* middle(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast!=NULL and fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
+    
+    ListNode* merge(ListNode* left, ListNode* right){
+        if(left==NULL) return right;
+        if(right==NULL) return left;
+        ListNode* ans= new ListNode(-1);
+        ListNode* tmp=ans;
+        while(left!=NULL and right!=NULL){
+            if(left->val<right->val){
+                tmp->next=left;
+                tmp=left;
+                left=left->next;
+            }
+        else {
+            tmp->next=right;
+            tmp=right;
+            right=right->next;
+          }
+        }
+       
+        if(left==NULL) tmp->next=right;
+        if(right==NULL) tmp->next=left;
+        
+        return ans->next;
+        
+    }
+    
+    
+    
+    
+    
+    ListNode* sortList(ListNode* head) {
+       if(head==NULL || head->next==NULL) return head;
+        
+        ListNode* mid=middle(head);
+        
+        ListNode* left=head;
+        ListNode* right=mid->next;
+        mid->next=NULL;
+        
+        left=sortList(left);
+        right=sortList(right);
+        
+       return merge(left,right);
     }
 };
