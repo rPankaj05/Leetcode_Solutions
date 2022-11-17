161dd96bbd101a0e0bd0ac68a359dd032269568b
rPankaj05 <108936363+rPankaj05@users.noreply.github.com>
2022-11-18 03:10:57 +0530

    Time: 19 ms (77.77%), Space: 20.9 MB (80.82%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
 class Solution {
     int maxDepth(TreeNode* root){
          if(root==NULL) return 0;
@@ -27,5 +28,24 @@ public:
         bool r=isBalanced(root->right);
         if(!r or !l) return false;
         return true;
+    }
+};
+
+*/
+
+class Solution {
+    int maxDepth(TreeNode* root){
+         if(root==NULL) return 0;
+        int lh=maxDepth(root->left);
+        int rh=maxDepth(root->right);
+        if(lh==-1 or rh==-1) return -1;
+        if(abs(lh-rh)>1) return -1;
+        return 1+max(lh,rh);
+    }
+    
+public:
+    bool isBalanced(TreeNode* root) {
+        if(maxDepth(root)!=-1) return true;
+        return false;
     }
 };
