ade886eda9cb0eda56a843f3ee6dc755bca3e07f
rPankaj05 <108936363+rPankaj05@users.noreply.github.com>
2022-12-08 02:08:52 +0530

    Added solution - LeetHub

--- 3 Divisors - GFG/3-divisors.cpp
+++ 3 Divisors - GFG/3-divisors.cpp
@@ -8,24 +8,17 @@ class Solution
     private: 
     
     
-bool isPrime(int n)
-{
-    if (n <= 1)
-        return false;
-    if (n <= 3)
-        return true;
+   bool isPrime(int n)
+    {  
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
  
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
- 
-    for (int i = 5; i * i <= n; i = i + 6)
-        if (n % i == 0 || n % (i + 2) == 0)
+      for (int i = 5; i * i <= n; i = i + 6)
+          if (n % i == 0 || n % (i + 2) == 0)
             return false;
- 
-    return true;
-}
-    
-    
+          return true;
+     }   
     
 public:
     vector<int> threeDivisors(vector<long long> query, int q)
