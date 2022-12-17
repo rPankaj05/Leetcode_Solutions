52423124e5f0c6ffa8a407b04602032b9b4ddc67
rPankaj05 <108936363+rPankaj05@users.noreply.github.com>
2022-12-18 00:49:35 +0530

    Added solution - LeetHub

---
+++ A Game of LCM - GFG/a-game-of-lcm.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long maxGcd(int N) {
+        long long int n=N;
+        int cnt=1;
+        long long int ans=n;
+        while(n-->2){
+            if(__gcd(ans,n)==1){
+                ans*=n;
+                cnt++;
+            }
+          if(cnt==4) break;
+        }
+       
+        return  ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.maxGcd(N) << "\n";
+    }
+}
+// } Driver Code Ends
