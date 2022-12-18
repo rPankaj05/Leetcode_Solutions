1391ec78c4cba8afabc2ac341c25d4644a24b42d
rPankaj05 <108936363+rPankaj05@users.noreply.github.com>
2022-12-19 03:23:32 +0530

    Time: 33 ms (26.75%), Space: 17.2 MB (99.49%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -1,21 +1,5 @@
+//solution 1 in O(n) and O(n) and sol 2 is after 1st 
 /*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
-
 class Solution {
 public:
     Node* connect(Node* root) {
@@ -33,8 +17,36 @@ public:
                 if(temp->left) q.push(temp->left); 
                 if(temp->right) q.push(temp->right);
             }
+        } 
+        return root;
+    }
+};  */
+
+
+// solution 2 in O(n) and constant space
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL) return NULL;
+        Node* curr=root;
+        while(curr!=NULL){
+            Node* dummy=new Node(-1);
+            Node* temp=dummy;
+            while(curr!=NULL){
+
+                if(curr->left){
+                    temp->next=curr->left;
+                    temp=temp->next;
+                }
+                
+                if(curr->right){
+                    temp->next=curr->right;
+                    temp=temp->next;
+                }
+                curr=curr->next;
+            }
+            curr=dummy->next;
         }
-        
         return root;
     }
 };
