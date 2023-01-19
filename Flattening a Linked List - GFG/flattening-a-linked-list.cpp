a56dbf40fc889af1a28980c747c5ec39d82b7a37
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2023-01-20 00:50:15 +0530

    Added solution - LeetHub

--- Flattening a Linked List - GFG/flattening-a-linked-list.cpp
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -1,21 +1,20 @@
 //{ Driver Code Starts
-//Initial Template for C++
-
 #include <bits/stdc++.h>
-using namespace std;
 
-struct Node {
-    int data;
-    struct Node * next;
-    struct Node * bottom;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-        bottom = NULL;
-    }
-
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
 };
+
+using namespace std;
 
 void printList(Node *Node)
 {
@@ -26,8 +25,74 @@ void printList(Node *Node)
     }
 }
 
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
 
 // } Driver Code Ends
+
+
 /* Node structure  used in the program
 
 struct Node{
@@ -44,94 +109,45 @@ struct Node{
 };
 */
 
-class Solution {
+/*  Function which returns the  root of 
+    the flattened linked list. */
     
-    Node* merge(Node* head1,Node* head2){
-        // if(!head1 && !head2) return nullptr;
-        if(!head1) return head2;
-        if(!head2) return head1;
-        if(head1->data<=head2->data){
-            head1->bottom=merge(head1->bottom,head2);
-            return head1;
-        }
-        head2->bottom=merge(head2->bottom,head1);
-        return head2;
-    }
-    
-    
-public:
-    Node *flatten(Node *root)
-    {
-        if(root==nullptr || root->next==nullptr) return root;
-        return merge(flatten(root->next),root);
-    }
-};
-
-
-//{ Driver Code Starts.
-
-int main(void) {
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m, flag = 1, flag1 = 1;
-        struct Node * temp = NULL;
-        struct Node * head = NULL;
-        struct Node * pre = NULL;
-        struct Node * tempB = NULL;
-        struct Node * preB = NULL;
-        cin >> n;
-        int work[n];
-        for (int i = 0; i < n; i++)
-            cin >> work[i];
-        for (int i = 0; i < n; i++) {
-            m = work[i];
-            --m;
-            int data;
-            scanf("%d", &data);
-            temp = new Node(data);
-            temp->next = NULL;
-            temp->bottom = NULL;
-
-            if (flag) {
-                head = temp;
-                pre = temp;
-                flag = 0;
-                flag1 = 1;
+    Node* merge(Node* a,  Node* b){
+        Node* temp= new Node(-1);
+        Node* res=temp;
+        
+        while(a!=NULL and b!=NULL){
+            if(a->data<=b->data){
+                temp->bottom=a;
+                temp=temp->bottom;
+                a=a->bottom;
             }
             else {
-                pre->next = temp;
-                pre = temp;
-                flag1 = 1;
-            }
-            for (int j = 0; j < m; j++) {
-
-                int temp_data;
-                scanf("%d", &temp_data);
-                tempB = new Node(temp_data);
-
-                if (flag1) {
-                    temp->bottom = tempB;
-                    preB = tempB;
-                    flag1 = 0;
-                }
-                else {
-                    preB->bottom = tempB;
-                    preB = tempB;
-                }
+                 temp->bottom=b;
+                temp=temp->bottom;
+                b=b->bottom;
             }
         }
-        Node *fun = head;
-        Node *fun2 = head;
-
-        Solution ob;
-        Node* root = ob.flatten(head);
-        printList(root);
-        cout << endl;
-
+        
+        if(a!=NULL) temp->bottom=a;
+        else temp->bottom=b;
+        
+        return res->bottom;
+        
     }
-    return 0;
+    
+    
+    
+    
+    
+Node *flatten(Node *root)
+{
+   if(root==NULL || root->next==NULL) return root;
+   
+   root->next=flatten(root->next);
+   
+   root=merge(root,root->next);
+   
+   return root;
 }
 
-// } Driver Code Ends
