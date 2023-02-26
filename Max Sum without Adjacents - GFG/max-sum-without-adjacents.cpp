3f466d28c2112f00dc7187d8c5c3c21fa25354e5
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2023-02-27 02:57:45 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -6,44 +6,34 @@ using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
-
-//  recursive solution 
-/*
 class Solution{
+    
+    private:
+      
+      int solve(int *arr, int n,vector<int> &dp){
+          
+          if(n<0) return 0 ;
+          if(n==0) return arr[0];
+         
+          if(dp[n]!=-1) return dp[n];
+           
+          int includeAns=solve(arr,n-2,dp)+arr[n];
+          int excludeAns=solve(arr,n-1,dp);
+          
+          dp[n]=max(includeAns,excludeAns);
+          
+          return dp[n];
+      }
+    
+    
 public:	
 	// calculate the maximum sum with out adjacent
-	int solve(int *arr, int n) {
-	    if(n<0) return 0;
-	    if(n==0) return arr[0];
-	    int take=arr[n]+solve(arr,n-2);
-	    int notTake=solve(arr,n-1);
-	    return max(take,notTake);
-	}
+	int findMaxSum(int *arr, int n) {
+	    vector<int> dp(n,-1);
+	    return solve(arr,n-1,dp);
 	
-	int findMaxSum(int *arr, int n) {
-	    int ans=0;
-	   ans=  mx(arr,n-1);
-	   return ans;
-
 	}
 };
-*/
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-class Solution{
-public:	
-	int findMaxSum(int *arr, int n) {
-	   vector<int> dp(n,0);
-	    dp[0]=arr[0];
-	    for(int i=1;i<n;i++){
-	        dp[i]=max(arr[i]+dp[i-2],dp[i-1]);
-	    }
-	    return dp[n-1];
-
-	}
-};
-
-
 
 //{ Driver Code Starts.
 
