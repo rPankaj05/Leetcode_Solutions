8a6145df18e8473d5d370757726f767607bcbf5b
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2023-02-27 17:25:55 +0530

    Added solution - LeetHub

---
+++ Remove all characters other than alphabets - GFG/remove-all-characters-other-than-alphabets.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    string removeSpecialCharacter(string s) {
+       string ans;
+       for(auto it:s){
+           if((it>='a' and it<='z') or it>='A' and it<='Z') ans+=it;
+       }
+       
+       if(ans=="") return "-1";
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Solution ob;
+        cout << ob.removeSpecialCharacter(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
