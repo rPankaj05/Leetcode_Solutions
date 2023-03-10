f5a9242734c43e5f57389faa288c27def2688aee
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2023-03-10 21:39:11 +0530

    Added solution - LeetHub

---
+++ Maximum Triplet product - GFG/maximum-triplet-product.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for CPP
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    long long maxTripletProduct(long long arr[], int n)
+    {
+    	long long mx1=INT_MIN, mx2=INT_MIN, mx3=INT_MIN;
+    	long long mn1=INT_MAX, mn2=INT_MAX;
+    	
+    	for(int i=0;i<n;i++){
+    	    if(arr[i]>=mx1){
+    	        mx3=mx2;
+    	        mx2=mx1;
+    	        mx1=arr[i];
+    	    }
+    	   
+    	   else if(arr[i]>=mx2) {
+    	       mx3=mx2;
+    	       mx2=arr[i];
+    	    }
+    	    
+    	   else if(arr[i]>=mx3){
+    	       mx3=arr[i];
+    	   }
+    	   
+    	  if(arr[i]<=mn1){
+    	      mn2=mn1;
+    	      mn1=arr[i];
+    	  }
+    	  else if(arr[i]<=mn2) mn2=arr[i];
+    	}
+    	
+      long long ans1=mx1*(mx2*mx3);
+      long long ans2=mx1*(mn1*mn2);
+      
+      return max(ans1,ans2);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,i;
+	    cin>>n; long long arr[n];
+	    for(i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution ob;
+	    cout <<ob.maxTripletProduct(arr, n);
+	    cout<<"\n";
+	}
+    return 0;
+}
+// } Driver Code Ends
