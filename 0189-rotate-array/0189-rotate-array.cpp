9a7473a7d52f293ec94dbd21bb7836650a3fa4d2
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2023-03-24 17:16:24 +0530

    Time: 31 ms (73.44%), Space: 25.7 MB (30.55%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> tmp(n);
+        for(int i=0;i<n;i++){
+            tmp[(i+k)%n]=nums[i];
+        }
+         nums=tmp;
+    }
+};
