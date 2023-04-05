06e8c67a76dc25ad96089a4d26b53024df5d98c1
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2023-04-05 17:20:40 +0530

    Added solution - LeetHub

---
+++ Count Special Numbers - GFG/count-special-numbers.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countSpecialNumbers(int n, vector<int> arr) {
+        unordered_map<int,int> mp;
+        for(auto it:arr) mp[it]++;
+        int ans=0;
+        for(auto it:mp){
+            if(it.second>1) ans+=it.second;
+            else{
+                for(int i=0;i<n;i++){
+                  if(arr[i]>=it.first) continue;  
+                  else{
+                      if(it.first%arr[i]==0) { ans++; break;}
+                  }
+             }
+         }   
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+
+        Solution ob;
+        cout << ob.countSpecialNumbers(N, arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
