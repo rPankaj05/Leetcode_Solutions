8ebaec00c19853c0555495f2006e2b2528a264bf
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2023-04-18 22:33:52 +0530

    Time: 4 ms (38.78%), Space: 6.2 MB (85.93%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans="";
+        int n1=word1.size();
+        int n2=word2.size();
+        int i=0,j=0;
+        while(i<n1 and j<n2){
+            ans.push_back(word1[i++]);
+            ans.push_back(word2[j++]);
+        }
+        while(i<n1) ans.push_back(word1[i++]);
+        while(j<n2) ans.push_back(word2[j++]);
+        return ans;
+    }
+};
