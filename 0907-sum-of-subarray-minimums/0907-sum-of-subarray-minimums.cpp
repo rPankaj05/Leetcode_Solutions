6be2b3f9e27a1eeebb76d8f951872ee0181e93f8
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2024-01-20 16:54:41 +0530

    Time: 113 ms (18.65%), Space: 45.5 MB (14.08%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,66 @@
+ void nextSmallerElement(int n,vector<int> &arr,vector<int> &nextSmaller){
+    stack<int> st;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() and arr[st.top()]>=arr[i]) st.pop();
+        if(!st.empty()) nextSmaller[i]=st.top();
+        else nextSmaller[i]=-1;
+        st.push(i);
+   }
+}
+
+void prevSmallerElement(int n,vector<int> &arr,vector<int> &prevSmaller){
+    stack<int> st;
+    for(int i=0;i<n;i++){
+        while(!st.empty() and arr[st.top()]>arr[i]) st.pop();
+        if(!st.empty()) prevSmaller[i]=st.top();
+        else prevSmaller[i]=-1;
+        st.push(i);
+   }
+}
+
+
+int mod=1e9+7;
+
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        long long int ans=0;
+        vector<int> nextSmaller(n,-1);
+        vector<int> prevSmaller(n,-1);
+        nextSmallerElement(n,arr,nextSmaller);
+        prevSmallerElement(n,arr,prevSmaller);
+        
+        for(int i=0;i<n;i++){
+           long long int rightd=n-i;
+           long long int leftd=i+1;
+           if(nextSmaller[i]!=-1) rightd=nextSmaller[i]-i;
+           if(prevSmaller[i]!=-1) leftd=i-prevSmaller[i];
+           ans+=((((rightd%mod)*(leftd%mod))%mod)*arr[i])%mod;
+           ans=ans%mod;
+        }
+        
+        return ans;
+    }
+};
+
+
+
+
+// int mod=1e9+7;
+// class Solution {
+// public:
+//     int sumSubarrayMins(vector<int>& arr) {
+//         int n=arr.size();
+//         int ans = 0;
+//         for (int i = 0; i < n; i++) {
+//             int min_ele = arr[i];
+//             for (int j = i; j < n; j++) {
+//                 min_ele = min(min_ele, arr[j]);
+//                 ans += min_ele;
+//                 ans=ans%mod;
+//             }
+//         }
+//         return ans%mod;
+//     }
+// };
