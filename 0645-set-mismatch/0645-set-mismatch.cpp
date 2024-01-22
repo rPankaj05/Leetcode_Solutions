db2b68cddbf0d84125f5f79b2d6ed3b16f57442c
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2024-01-22 13:28:39 +0530

    Time: 17 ms (93.49%), Space: 24.7 MB (34.75%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> v(n,0);
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            v[nums[i]-1]++;
+         }
+        for(int i=0;i<n;i++){
+            if(v[i]==2) ans.push_back(i+1);
+        }
+        for(int i=0;i<n;i++){
+            if(v[i]==0) ans.push_back(i+1);
+        }
+        return ans;
+    }
+    
+};
