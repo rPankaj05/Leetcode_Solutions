06ff83c1ea98b549cbdd81ccf069dc68b645993d
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2024-01-22 13:30:20 +0530

    Time: 27 ms (58.87%), Space: 24.8 MB (34.62%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -8,11 +8,10 @@ public:
             v[nums[i]-1]++;
          }
         for(int i=0;i<n;i++){
-            if(v[i]==2) ans.push_back(i+1);
-        }
-        for(int i=0;i<n;i++){
             if(v[i]==0) ans.push_back(i+1);
+            if(v[i]==2) ans.insert(ans.begin(),i+1);
         }
+
         return ans;
     }
     
