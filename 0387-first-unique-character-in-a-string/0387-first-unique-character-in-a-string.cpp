dab5fea4777450c41f41f1aa366df41edc222180
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2024-02-05 21:40:14 +0530

    Time: 42 ms (27.58%), Space: 13 MB (17.78%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,12 +1,25 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         int n=s.size();
+//         vector<int> vec(256,0);
+//         for (auto it:s) vec[it]++;
+//         for(int i=0;i<n;i++){
+//             if(vec[s[i]]==1) return i;
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int n=s.size();
-        vector<int> vec(256,0);
-        for (auto it:s) vec[it]++;
-        for(int i=0;i<n;i++){
-            if(vec[s[i]]==1) return i;
-        }
-        return -1;
-    }
+    unordered_map<char, int> freq;
+    for(char c:s) freq[c]++;
+    for(int i=0;i<s.size();i++) {
+        if (freq[s[i]] == 1) return i;
+     }
+    return -1;
+  }
+
 };
