7ad2302dcdb3524c314758b1638b35f5059dad24
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2024-09-12 11:01:18 +0530

    Time: 49 ms (46.08%), Space: 34 MB (58.62%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -0,0 +1,23 @@
+bool isValidString(vector<int> &v,string word){
+            for(auto ch:word){
+                if(v[ch-'a']==0) return false;
+            }
+    return true;
+}
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        int ans=0;
+        vector<int> v(26,0);
+        for(auto it:allowed){
+            v[it-'a']=1;
+        }
+        for (auto word:words){
+           if(isValidString(v,word)) ans++; 
+        }
+        cout<<"ans"<<" "<<ans;
+        return ans;
+    }
+};
+
